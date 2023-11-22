43872f83af8c05c232db86c6b336983abee84da8
annatrajano <annatrajano95@gmail.com@example.com>
2023-11-22 18:15:59 -0300

    feat: :sparkles: add a new state to wineQualityPredict function

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -15,13 +15,16 @@ import styles from "./Home.module.css";
 
 export default function Home() {
   const [values, setValues] = useState({});
+  const [quality, setQuality] = useState();
 
   async function handleSubmit() {
     const objectValues = Object.keys(values).map((k) => values[k]);
     const arrOfNum = objectValues.map((str) => {
       return Number(str);
     });
-    const response = await wineQualityPredict({ "data": arrOfNum });
+    const response = await wineQualityPredict({ data: arrOfNum });
+    setQuality(response);
+    setValues({})
     console.log(response);
   }
 
@@ -34,7 +37,7 @@ export default function Home() {
       id: 1,
       name: "fixed_acidity",
       type: "number",
-      placeholder: `7`,
+      placeholder: `4.5 to 10`,
       disabled: false,
       label: "Fixed Acidity",
       required: true,
@@ -44,7 +47,7 @@ export default function Home() {
       id: 2,
       name: "volatile_acidity",
       type: "number",
-      placeholder: `0.27`,
+      placeholder: `0.1 to 0.75`,
       disabled: false,
       label: "Volatile Acidity",
       required: true,
@@ -54,7 +57,7 @@ export default function Home() {
       id: 3,
       name: "citric_acid",
       type: "number",
-      placeholder: `0.36`,
+      placeholder: `0 to 0.7`,
       disabled: false,
       label: "Citric Acid",
       required: true,
@@ -64,7 +67,7 @@ export default function Home() {
       id: 4,
       name: "residual_sugar",
       type: "number",
-      placeholder: `20.7`,
+      placeholder: `0 to 25`,
       disabled: false,
       label: "Residual Sugar",
       required: true,
@@ -74,7 +77,7 @@ export default function Home() {
       id: 5,
       name: "chlorides",
       type: "number",
-      placeholder: `0.045`,
+      placeholder: `0.02 to 0.2`,
       disabled: false,
       label: "Chlorides",
       required: true,
@@ -84,7 +87,7 @@ export default function Home() {
       id: 6,
       name: "free_sulfur_dioxide",
       type: "number",
-      placeholder: `45`,
+      placeholder: `0 to 150`,
       disabled: false,
       label: "Free Sulfur Dioxide",
       required: true,
@@ -94,7 +97,7 @@ export default function Home() {
       id: 7,
       name: "total_sulfur_dioxide",
       type: "number",
-      placeholder: `170`,
+      placeholder: `0 to 250`,
       disabled: false,
       label: "Total Sulfur Dioxide",
       required: true,
@@ -104,7 +107,7 @@ export default function Home() {
       id: 8,
       name: "density",
       type: "number",
-      placeholder: `1.001`,
+      placeholder: `0.9 to 1`,
       disabled: false,
       label: "Density",
       required: true,
@@ -114,7 +117,7 @@ export default function Home() {
       id: 9,
       name: "ph",
       type: "number",
-      placeholder: `3`,
+      placeholder: `2.75 to 3.75`,
       disabled: false,
       label: "pH",
       required: true,
@@ -124,7 +127,7 @@ export default function Home() {
       id: 10,
       name: "sulphates",
       type: "number",
-      placeholder: `0.45`,
+      placeholder: `0.25 to 1`,
       disabled: false,
       label: "Sulphates",
       required: true,
@@ -134,7 +137,7 @@ export default function Home() {
       id: 11,
       name: "alcohol",
       type: "number",
-      placeholder: `8.8`,
+      placeholder: `8.5 to 14`,
       disabled: false,
       label: "Alcohol",
       required: true,
@@ -145,7 +148,7 @@ export default function Home() {
     <div className={styles.home__container}>
       <Header />
       <Background />
-      <section>
+      <section className={styles.section__container}>
         <form className={styles.form__container}>
           {firstInputSection.map((input) => (
             <FormInput
@@ -157,14 +160,15 @@ export default function Home() {
           ))}
           <div className={styles.bttn__container}>
             <button
-              className={styles.bttn__create}
+              className={styles.bttn__quality}
               type="button"
               onClick={handleSubmit}
             >
-              <p className={styles.text__bttn}>Cadastrar</p>
+              <p className={styles.text__bttn}>Avaliar</p>
             </button>
           </div>
         </form>
+        {quality ? <h2 className={styles.text__quality} >{quality}</h2> : ""}
       </section>
       <Footer />
     </div>
