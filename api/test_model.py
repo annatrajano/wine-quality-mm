febd81ba5cc3bb51b66f48b39bda5bf51d871373
annatrajano <annatrajano95@gmail.com@example.com>
2023-12-08 16:50:03 -0300

    [add] test_model.py

---
+++ api/test_model.py
@@ -0,0 +1,30 @@
+from model.evaluator import Evaluator
+from model.loader import Loader
+from model.model import Model
+
+
+evaluator = Evaluator()
+loader = Loader()
+model = Model()
+
+# Parameters   
+url = "winequality-white.csv"
+
+# Charge data
+dataset = loader.charge_data(url)
+
+# Separating input and output data
+X = dataset.drop('quality', axis=1)
+Y = dataset['quality']
+
+
+def test_model_rf():  
+    # Importing the Random Forest model
+    rf_path = 'model.pkl'
+    model_rf = Model.load_model(rf_path)
+    
+    # Obtaining Random Forest metric
+    accuracy_rf = evaluator.evaluate(model_rf, X, Y)
+    
+    # Testing Random Forest metric
+    assert accuracy_rf >= 0.35
